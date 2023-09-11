ecc43e24dae7c45405b31bc02d46507326e28baf
sonie <garachu.muthoni@gmail.com>
2023-09-11 03:07:01 -0700

    initial commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "shell.h"
+/**
+ * main - prints the date the project commenced.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("This simple shell was created from the 10th of Sept 2023\n");
+	return (0);
+}
