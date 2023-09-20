681e855fc78a92f7d0f8967115255489453345fd
sonie <garachu.muthoni@gmail.com>
2023-09-20 05:45:06 -0700

    implement env builtin command

--- shell.h
+++ shell.h
@@ -8,7 +8,8 @@
 void line_to_array(char *user_input, char *program_name);
 void execute_tokens(char **tokenarr, char *program_name);
 void free_tokenarr(char **tokenarr, int i, char *user_input_cp);
-void get_line_function(char *program_name);
-void process_user_input(char *user_input, char *program_name);
+void get_line_function(char *program_name, char **envp);
+void process_user_input(char *user_input, char *program_name, char **envp);
 void handle_error(char *user_input);
+void env_builtin(char **envp);
 #endif
