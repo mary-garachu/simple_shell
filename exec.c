213d596c780e56b7b7e3260e654c46014c7c5fed
kipngenokevin <kipngenokevin254@gmail.com>
2023-09-21 16:22:51 +0300

    checking _strcmp function

--- exec.c
+++ exec.c
@@ -103,14 +103,14 @@ void get_line_function(char *program_name, char **envp)
 				handle_error(user_input);
 			}
 		}
-		if (_strcmp(user_input, "exit\n") == 0)
+		if (read_input != -1)
 		{
-			free(user_input);
-			exit(last_command_status);
-		}
-
-		else
-		{
+			if (_strcmp(user_input, "exit\n") == 0)
+			{
+				free(user_input);
+				exit(last_command_status);
+			}
+			else
 			process_user_input(user_input, program_name, envp);
 			input_size = 0;
 			free(user_input);
