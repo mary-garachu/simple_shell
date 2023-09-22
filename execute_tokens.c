da532a10bbe0be63dfc607f83c7e55ba3b81a0f7
kipngenokevin <kipngenokevin254@gmail.com>
2023-09-22 13:29:00 +0300

    checking if command exists in PATH function has been added

--- execute_tokens.c
+++ execute_tokens.c
@@ -10,13 +10,15 @@
 void execute_tokens(char **tokenarr, char *program_name, char **envp)
 {
 	int i = 0;
+	char *str = NULL;
 
-	if (tokenarr[0] == NULL)
+	str = check_environment(tokenarr[0], envp);
+	if (str == NULL)
 	{
-		exit(EXIT_FAILURE);
 		free(tokenarr);
+		exit(EXIT_FAILURE);
 	}
-	if (execve(tokenarr[0], tokenarr, envp) == -1)
+	if (execve(str, tokenarr, envp) == -1)
 	{
 		perror(program_name);
 		exit(EXIT_FAILURE);
@@ -27,4 +29,65 @@ void execute_tokens(char **tokenarr, char *program_name, char **envp)
 		i++;
 	}
 	free(tokenarr);
+}
+/**
+ * check_environment - checks if the command is found in environment
+ * @command: command passed
+ * @envp: environment variables
+ * Return: full command or NULL
+*/
+char *check_environment(char *command, char **envp)
+{
+	char *path_variable = NULL, *path_token = NULL;
+	char *full_path = NULL;
+	int i = 0;
+
+	if (command == NULL || envp == NULL)
+		return (NULL);
+	for (i = 0; envp[i] != NULL; i++)
+	{
+		if (_strncmp(envp[i], "PATH=", 5) == 0)
+		{
+			path_variable = envp[i] + 5;
+			break;
+		}
+	}
+	if (path_variable == NULL)
+		return (NULL);
+	path_token = strtok(path_variable, ":");
+	while (path_token != NULL)
+	{
+		full_path = malloc(_strlen(path_token) + _strlen(command) + 2);
+		if (full_path == NULL)
+			return (NULL);
+		sprintf(full_path, "%s/%s", path_token, command);
+		if (access(full_path, F_OK) != -1)
+			return (full_path);
+		free(full_path);
+		path_token = strtok(NULL, ":");
+	}
+	return (command);
+}
+/**
+ * _strncmp - similar to strncmp
+ * @s1: first string
+ * @s2: second string
+ * @n: size to check
+ * Return: 0 success -1 fail
+*/
+int _strncmp(const char *s1, const char *s2, size_t n)
+{
+	if (s1 != NULL && s2 != NULL)
+	{
+		while (n > 0 && *s1 && (*s1 == *s2))
+		{
+			s1++;
+			s2++;
+			n--;
+		}
+		if (n == 0)
+			return (0);
+		return (*s1 - *s2);
+	}
+	return (0);
 }
--- shell.h
+++ shell.h
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <stddef.h>
 void line_to_array(char *user_input, char *program_name, char **envp);
 void execute_tokens(char **tokenarr, char *program_name, char **envp);
 void free_tokenarr(char **tokenarr, int i, char *user_input_cp);
@@ -17,4 +18,8 @@ int _strcmp(const char *str1, const char *str2);
 int is_whitespace_string(const char *str);
 int is_empty_input(const char *user_input, int is_pipe);
 void trim_whitespace(char *str);
+char *check_environment(char *command, char **envp);
+int _strncmp(const char *s1, const char *s2, size_t n);
+size_t _strlen(const char *str);
+char *_sprintf(const char *str1, const char *str2, const char *str3);
 #endif
