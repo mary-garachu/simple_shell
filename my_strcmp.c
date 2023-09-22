284fd3333d7860b3602ad5f5e95ac07c71fda728
kipngenokevin <kipngenokevin254@gmail.com>
2023-09-22 10:52:28 +0300

    I have checked for NULL in the _strcmp()

--- my_strcmp.c
+++ my_strcmp.c
@@ -7,15 +7,19 @@
  */
 int _strcmp(const char *str1, const char *str2)
 {
-	while (*str1 != '\0' && *str2 != '\0')
+	if (str1 != NULL && str2 != NULL)
 	{
-		if (*str1 != *str2)
+		while (*str1 != '\0' && *str2 != '\0')
 		{
-			return (*str1 - *str2);
+			if (*str1 != *str2)
+			{
+				return (*str1 - *str2);
+			}
+			str1++;
+			str2++;
 		}
-		str1++;
-		str2++;
+		return (*str1 - *str2);
 	}
-
-	return (*str1 - *str2);
+	else
+		return (1);
 }
